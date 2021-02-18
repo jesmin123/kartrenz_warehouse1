03dac50516422b03bcab6dec16190bed5b7d9b52
jesmin123 <jesminjames23@gmail.com>
2021-02-18 15:10:25 +0530

    ui design

--- lib/ui/widgets/Submitted_Form_widget/expand_details-page.dart
+++ lib/ui/widgets/Submitted_Form_widget/expand_details-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kartenz/constants/constant_widgets.dart';
 
 class ExpandDetailPage extends StatefulWidget {
   @override
@@ -8,6 +9,22 @@ class ExpandDetailPage extends StatefulWidget {
 class _ExpandDetailPageState extends State<ExpandDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: appBar(context, "Cars"),
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
   }
+}
+
+Widget carDetails(String tittle1, String tittle2){
+  return Column(
+    children: [
+      Text(tittle1, style: AppFontStyle.headingTextStyle2(APP_BLACK_COLOR, textSize: 16.0)),
+      Text(tittle2, style: AppFontStyle.regularTextStyle(APP_BLACK_COLOR, textSize: 14.0),)
+    ],
+  );
 }
